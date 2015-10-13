74691df61d2bb6e8c44e8e728fd733f5b1a10787
Andreas Hammar <andreas.hammar@jayway.com>
2015-10-13 13:20:10 +0200

    started on bg task

---
+++ src/UwpDeepDive/UwpDeepDive.Background/AppBgTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace UwpDeepDive.Background
+{
+    public sealed class AppBgTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var toastNotifier = ToastNotificationManager.CreateToastNotifier();
+            var templateContent = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            
+            toastNotifier.Show();
+        }
+    }
+}
