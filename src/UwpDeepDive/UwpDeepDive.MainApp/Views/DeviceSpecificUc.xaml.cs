07f30e3fd124d1f86ea401d396c18057ec4be31a
Andreas Hammar <andreas.hammar@jayway.com>
2015-10-15 10:47:00 +0200

    moved device-XAML to views

--- src/UwpDeepDive/UwpDeepDive.MainApp/Views/DeviceSpecificUc.xaml.cs
+++ src/UwpDeepDive/UwpDeepDive.MainApp/Views/DeviceSpecificUc.xaml.cs
@@ -1,6 +1,6 @@
 ﻿// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace UwpDeepDive.MainApp
+namespace UwpDeepDive.MainApp.Views
 {
     public sealed partial class DeviceSpecificUc
     {
